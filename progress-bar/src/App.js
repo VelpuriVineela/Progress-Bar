a6564f5c12d5d9e00186cd63b1f3f0c271d82ecb
velpuri vineela <vineelavelpuri@gmail.com>
2024-01-01 20:43:22 +0530

    Progress Bar

---
+++ progress-bar/src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import './App.css';
+import Progressbar from "./Progressbar"
+import "./style.css";
+
+
+function App() {
+  return (
+    <div className="App">
+     < Progressbar /> 
+    </div>
+  );
+}
+
+export default App;
